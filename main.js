49b67eef348cb39f941cd61deef3cff105ce3d0a
Farhan Akrami <farhanak517@gmail.com>
2025-10-19 22:05:00 +0530

    designed the layout and initiated working with js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 console.log("Main.js working");
 
-console.log("heyyy");
+url =
+  "https://api.currencyapi.com/v3/latest?apikey=cur_live_jzSIgxgvzIjIKcVzQ8LHMY8q09D2tyEn2WA1ElzU";
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("button is clicked");
+});
+
+const tablebody = document.querySelector("tbody");
+tablebody.innerHTML = `
+<tr>
+<td>Data 1</td>
+<td>Data 2</td>
+<td>Data 3</td>
+</tr>
+`;
